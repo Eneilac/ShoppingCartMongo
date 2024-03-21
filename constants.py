f8e3b48bc9f347583aabc08923f5af1414ed0acd
Eneilac <eduarneila@gmail.com>
2024-03-21 16:01:45 +0100

    conf pc casa

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 import os
 
 DB_USER = os.getenv("DB_USER", "root")
-PASSWORD = os.getenv("DB_PASSWORD", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "root")
 CLUSTER_NAME = os.getenv("CLUSTER_NAME", "cluster0")
-DATABASE_NAME = os.getenv("DATABASE_NAME", "database")
+DATABASE_NAME = os.getenv("DATABASE_NAME", "shoppingCart")
